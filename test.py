1aad22b486f14162a7305442de3ddfb920ac2775
Noah Gonzalez Lanchart <nzgl@MacBook-Pro-de-juan.local>
2019-10-26 21:03:04 +0200

    premire commit

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-permier = input("Premie chiffre : ")
-print(int(permier))
+#Une Variable c'est ce que te permet de stocke des informations, dans ce cas c'est le mot premier il stock le value que tu donne dans l'input
+premier = input("Premie chiffre : ")
+print(int(premier))
 deuxieme = input('deuxieme chiffre: ' )
 print(int(deuxieme))
 
-print(int(permier)*int(deuxieme))
+print(int(premier)*int(deuxieme))
